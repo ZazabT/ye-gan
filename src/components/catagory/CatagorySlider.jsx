3f965ced5187922336c0328fe1e8282ed3957125
ZazabT <157392321+ZazabT@users.noreply.github.com>
2024-10-20 21:43:02 +0300

    inhanced more

--- src/components/catagory/CatagorySlider.jsx
+++ src/components/catagory/CatagorySlider.jsx
@@ -3,12 +3,12 @@ const CatagorySlide = ({ catagory }) => {
     <div className="relative inline-block mx-2 transition-transform duration-300 hover:scale-105">
       {/* Icon on top */}
       <div className="flex flex-col items-center mb-2">
-        <i className={`${catagory.icon} text-2xl text-gray-700`} />
-        <h3 className="font-light text-sm text-center text-gray-90000">{catagory.name}</h3>
+        <i className={`${catagory.icon} text-3xl text-gray-700`} />
+        <h3 className="font-medium text-base text-center text-gray-900">{catagory.name}</h3>
       </div>
 
       {/* Hover description */}
-      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#50087b] text-white text-xs p-2 rounded opacity-0 hover:opacity-100 transition-opacity duration-300 z-1000">
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-[#50087b] text-white text-xs p-2 rounded opacity-0 hover:opacity-100 transition-opacity duration-300 z-10">
         {catagory.description}
       </div>
 
@@ -20,6 +20,7 @@ const CatagorySlide = ({ catagory }) => {
               href="#"
               className="inline-flex shrink-0 items-center gap-2 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
             >
+              {/* Add any additional tab navigation items here */}
             </a>
           </nav>
         </div>
